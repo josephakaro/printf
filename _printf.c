f5ff6f827b796b5c73b9c33b4d5803520bb02735
josephmarko <josephmmakaro@gmail.com>
2023-07-25 10:11:53 -0700

    Betty solved_1

--- _printf.c
+++ _printf.c
@@ -1,57 +1,56 @@
 #include "main.h"
-
 /**
  * _printf - Prints argument to the stdout
- *
  * @format: Identifiers.
- *
  * Return: Prints out the passed argument.
  */
-
-int _printf(const char * format, ...)
+int _printf(const char *format, ...)
 {
 	int count = 0;
+
 	va_list args;
- 	 int i = 0;
+	int i = 0;
 
-  va_start(args, format);
-  while (format[i] != '\0') {
-    if (format[i] == '%') {
-      switch (format[i + 1]) {
-        case 'c': {
-          char c = va_arg(args, int);
-          count += printf("%c", c);
-          i++;
-          break;
-        }
-        case 's': {
-          char *str = va_arg(args, char *);
-          count += printf("%s", str);
-          i++;
-          break;
-        }
-        case '%': {
-          count += printf("%%");
-          i++;
-          break;
-        }
-        case 'd':
-        case 'i': {
-          int in = va_arg(args, int);
-          count += printf("%d", in);
-          i++;
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    } else {
-      count += printf("%c", format[i]);
-    }
-    i++;
-  }
-
-  va_end(args);
-  return (count);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					{
+						count += printf("%c", va_arg(args, int));
+						break;
+					}
+				case 's':
+					{
+						count += printf("%s", va_arg(args, char *));
+						break;
+					}
+				case '%':
+					{
+						count += printf("\%");
+						break;
+					}
+				case 'd':
+				case 'i':
+					{
+						count += printf("%d", va_arg(args, int));
+						break;
+					}
+				default:
+					{
+						break;
+					}
+			}
+		}
+		else
+		{
+			count += printf("%c", format[i]);
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
