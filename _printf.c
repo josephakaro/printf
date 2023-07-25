336e65c2576afcb9dc4f3fabfb7838a4da6249ed
josephmarko <josephmmakaro@gmail.com>
2023-07-25 08:07:56 -0700

    _printf done!

--- _printf.c
+++ _printf.c
@@ -1,107 +1,57 @@
 #include "main.h"
 
 /**
- * _printf - Holds selected functions and prints it to stdout.
+ * _printf - Prints argument to the stdout
  *
- * @format: identifier.
+ * @format: Identifiers.
  *
- * Return:  Lenght of the string.
-*/
+ * Return: Prints out the passed argument.
+ */
 
-int _printf(const char * const format, ...)
+int _printf(const char * format, ...)
 {
-    int i = 0;
-    int len = 0;
-    va_list args;
+	int count = 0;
+	va_list args;
+ 	 int i = 0;
 
-    va_start(args, format);
-
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
-
-    int (*print_f[])(va_list) =
-    {
-        printf_string,
-        printf_char,
-        printf_37,
-        printf_int,
-        printf_dec,
-        printf_srev,
-        printf_rot13,
-        printf_bin,
-        printf_unsigned,
-        printf_oct,
-        printf_hex,
-        printf_HEX,
-        printf_exclusive_string,
-        printf_pointer
-    };
-
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            int type = format[i + 1];
-
-            switch(type)
-            {
-            case 's':
-                len += print_f[0](args);
-                break;
-            case 'c':
-                len += print_f[1](args);
-                break;
-            case '%':
-                len += print_f[2](args);
-                break;
-            case 'i':
-                len += print_f[3](args);
-                break;
-            case 'd':
-                len += print_f[4](args);
-                break;
-            case 'r':
-                len += print_f[5](args);
-                break;
-            case 'R':
-                len += print_f[6](args);
-                break;
-            case 'b':
-                len += print_f[7](args);
-                break;
-            case 'u':
-                len += print_f[8](args);
-                break;
-            case 'o':
-                len += print_f[9](args);
-                break;
-            case 'x':
-                len += print_f[10](args);
-                break;
-            case 'X':
-                len += print_f[11](args);
-                break;
-            case 'S':
-                len += print_f[12](args);
-                break;
-            case 'p':
-                len += print_f[13](args);
-                break;
-            default:
-                _putchar(format[i]);
-                len++;
-                i++;
-                break;
-            }
+  va_start(args, format);
+  while (format[i] != '\0') {
+    if (format[i] == '%') {
+      switch (format[i + 1]) {
+        case 'c': {
+          char c = va_arg(args, int);
+          count += printf("%c", c);
+          i++;
+          break;
         }
-        else
-        {
-            _putchar(format[i]);
-            len++;
-            i++;
+        case 's': {
+          char *str = va_arg(args, char *);
+          count += printf("%s", str);
+          i++;
+          break;
         }
+        case '%': {
+          count += printf("%%");
+          i++;
+          break;
+        }
+        case 'd':
+        case 'i': {
+          int in = va_arg(args, int);
+          count += printf("%d", in);
+          i++;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    } else {
+      count += printf("%c", format[i]);
     }
-    va_end(args);
-    return (len);
-}
+    i++;
+  }
 
+  va_end(args);
+  return (count);
+}
--- main.h
+++ main.h
@@ -8,27 +8,7 @@
 #include <unistd.h>
 
 
-extern int printf_pointer(va_list val);
-extern int printf_hex_aux(unsigned long int num);
-extern int printf_HEX_aux(unsigned int num);
-extern int printf_exclusive_string(va_list val);
-extern int printf_HEX(va_list val);
-extern int printf_hex(va_list val);
-extern int printf_oct(va_list val);
-extern int printf_unsigned(va_list args);
-extern int printf_bin(va_list val);
-extern int printf_srev(va_list args);
-extern int printf_rot13(va_list args);
-extern int printf_int(va_list args);
-extern int printf_dec(va_list args);
-extern int _strlen(char *s);
-extern int *_strcpy(char *dest, char *src);
-extern int _strlenc(const char *s);
-extern int rev_string(char *s);
-extern int _strlenc(const char *s);
-extern int printf_37(void);
 extern int printf_char(va_list val);
-extern int printf_string(va_list val);
 extern int _putchar(char c);
 extern int _printf(const char * const format, ...);
 
