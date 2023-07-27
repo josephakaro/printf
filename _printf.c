ee0e32ce85cc2eb4cacbd7e8bda57effa7f70304
nelsonwamalwa <wamalwanelson@gmail.com>
2023-07-27 14:08:55 -0700

    Betty Syle Edits on _printf.c done remaining the global variables issue

--- _printf.c
+++ _printf.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-FormatSpecifier arr[] =
-{
+FormatSpecifier arr[] = {
 	{"%s", printf_string},
-        {"%c", printf_char},
-        {"%%", printf_cent},
-        {"%d", printf_int},
+	{"%c", printf_char},
+	{"%%", printf_cent},
+	{"%d", printf_int},
 	{"%s", printf_string},
 	{"%c", printf_char},
 	{"%%", printf_cent},
@@ -30,37 +29,36 @@ FormatSpecifier arr[] =
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int i = 0, j, ln = 0;
+	va_list ap;
+	int i = 0, j, ln = 0;
 
-    va_start(ap, format);
+	va_start(ap, format);
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 
-    while (format[i] != '\0')
-    {
-        j = 4;
+	while (format[i] != '\0')
+	{
+	j = 4;
 
-        while (j >= 0)
-        {
-            if (format[i] == arr[j].place[0] && format[i + 1] == arr[j].place[1])
-            {
-                ln += arr[j].f(ap);
-                i = i + 2;
-                break;
-            }
-            j--;
-        }
+	while (j >= 0)
+	{
+	if (format[i] == arr[j].place[0] && format[i + 1] == arr[j].place[1])
+	{
+	ln += arr[j].f(ap);
+	i = i + 2;
+	break;
+	}
+	j--;
+	}
 
-        if (j < 0)
-        {
-            _putchar(format[i]);
-            ln++;
-            i++;
-        }
-    }
-
-    va_end(ap);
-    return (ln);
+	if (j < 0)
+	{
+	_putchar(format[i]);
+	ln++;
+	i++;
+	}
+	}
+	va_end(ap);
+	return (ln);
 }
