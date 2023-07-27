1aed921bd2ce9269d296b40508d99717ef6c278b
nelsonwamalwa <wamalwanelson@gmail.com>
2023-07-27 07:44:46 -0700

    _printf.c <<<<<<< HEAD and >>>>>>> 1e1ba78e95e734d5c9421aa04a614f1bd428f1da removal

--- printf_string.c
+++ printf_string.c
@@ -9,17 +9,10 @@
  */
 int printf_string(va_list v)
 {
-<<<<<<< HEAD
     char *str = va_arg(v, char *);
-    int ln = str ? strlen(str) : 8;
+    int ln = str ? strlen(str) : 6;
 
-    int i;
-=======
-	char *str = va_arg(v, char *);
-	int ln = str ? strlen(str) : 6;
->>>>>>> 1e1ba78e95e734d5c9421aa04a614f1bd428f1da
-
-    i = 0;
+    int i = 0;
 
     while (i < ln)
     {
