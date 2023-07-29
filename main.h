43e95823b796f09b2b574518bea5de7514dc55ba
josephmarko <josephmmakaro@gmail.com>
2023-07-29 07:08:57 +0200

    Main.h prototyping

---
+++ main.h
@@ -0,0 +1,60 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*** HEADER FILES ****/
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits>
+
+
+/*** STRUCT FORMAT ***/
+
+typedef struct format
+{
+    char *home;
+    int (*f)();
+} Specifiers;
+
+/*** PROTOTYPE FUNCTIONS ***/
+
+/*** TASK 0 ***/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int printf_char(va_list val);
+
+/*** TASK 1  */
+
+int printf_int(va_list val);
+int printf_int(va_list val);
+
+/*** TASK 2  */
+
+/*** TASK 3  */
+
+/*** TASK 4  */
+
+/*** TASK 5  */
+
+/*** TASK 6  */
+
+/*** TASK 7  */
+
+/*** TASK 8  */
+
+/*** TASK 9  */
+
+/*** TASK 10 */
+
+/*** TASK 11 */
+
+/*** TASK 12 */
+
+/*** TASK 13 */
+
+/*** TASK 14 */
+
+#endif
